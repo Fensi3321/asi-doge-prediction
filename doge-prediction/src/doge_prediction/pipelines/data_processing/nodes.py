c2284ddcce00d1d543cbe7d3916b43acf8bb3dda
lsugajski7 <lsugajski7@gmail.com>
2022-11-20 22:54:57 +0100

    Added kedro pipeline

--- doge-prediction/src/doge_prediction/pipelines/data_processing/nodes.py
+++ doge-prediction/src/doge_prediction/pipelines/data_processing/nodes.py
@@ -5,5 +5,8 @@ generated using Kedro 0.18.3
 
 import pandas as pd
 
-def hello_world(data: pd.DataFrame) -> pd.DataFrame:
+def max_date(data: pd.DataFrame) -> pd.DataFrame:
     return {'max_date': data['Date'].max()}
+
+def avg_high(data: pd.DataFrame) -> pd.DataFrame:
+    return {'avg_date': data['High'].mean()}
